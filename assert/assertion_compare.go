cf637e09acb390491efc12cb65bd4cd183433e38
Karine Valenca <valenca.karine@gmail.com>
2021-04-21 09:29:52 -0300

    minor fix

--- assert/assertion_compare.go
+++ assert/assertion_compare.go
@@ -10,7 +10,7 @@ func Equal(e1 interface{}, e2 interface{}) error {
 }
 
 func Nil(e1 interface{}) error {
-	if IsNil(e1) {
+	if isNil(e1) {
 		return nil
 	}
 	return fmt.Errorf(`Test failed: Expected "%s" to be nil, but got %s"`, e1, e1)
